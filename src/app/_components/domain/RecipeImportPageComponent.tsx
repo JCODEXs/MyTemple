2f713b0cba2f0134b12e766fc89ddb510cafac0b
jcodex <juansebastianescobar.vega@gmail.com>
2026-03-24 22:58:37 -0500

    1

--- src/app/_components/domain/RecipeImportPageComponent.tsx
+++ src/app/_components/domain/RecipeImportPageComponent.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
 "use client"
 
 import { useState, useMemo } from "react"
